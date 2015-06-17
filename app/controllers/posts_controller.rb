f2852017882de5ac6b6be65aa6f80d2051c174ac
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-06-16 17:59:29 -0700

    added routes and created home view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
 
-# Is a method to display the views for the index of post and images
+# Is a method displaying the landing page.
+    def home
+    end
+
+# Is a method to display the views for the index of post and images.
     def index
     end
 
-# is a method to display the individual page of the selected post
+# is a method to display the individual page of the selected post.
     def show
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+# rooting to the controller home
+root to: 'posts#home'
+
+resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
