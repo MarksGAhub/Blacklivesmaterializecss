442afb47f605361eb745ab0ab52b7beff1f67230
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-06-16 17:27:57 -0700

    Created post model, controller and header plus footer partials.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+
+# Is a method to display the views for the index of post and images
+    def index
+    end
+
+# is a method to display the individual page of the selected post
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+
+end
